67666fdad67c83772e8899f4f9b2ee798c2e6082
daniel7985 <danielbonilla@hotmail.com>
2022-01-14 20:26:48 -0500

    agregando funcionalidad

---
+++ src/main/java/com/unal/cronus/controller/login/StudentController.java
@@ -0,0 +1,32 @@
+package com.unal.cronus.controller.login;
+
+import com.unal.cronus.model.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/private/student")
+public class StudentController{
+
+    @Autowired
+    private SubjectService subjectService;
+
+    @GetMapping()
+    public String showStudentMainPage(){
+        return "student";
+    }
+
+    @PostMapping()
+    public String searchSubjects(@RequestParam("keyword") String keyword, Model model){
+        model.addAttribute("subjects",subjectService.searchSubjectsByName(keyword));
+        return "student";
+    }
+
+    @GetMapping("/subject/{code}")
+    public String showSubjectPage(@PathVariable("code") int code, Model model){
+        model.addAttribute("subject",subjectService.searchSubjectsByCode(code));
+        return "subjectdemo";
+    }
+}
