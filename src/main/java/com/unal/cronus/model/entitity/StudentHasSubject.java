b76c19cd1297bc32cc05735de27ea65fdb179ea4
sarodriguezcam <sarodriguezca@unal.edu.co>
2022-01-15 23:29:14 -0500

    Cambios pequeños a vistas, creación de StudentHasSubject.

---
+++ src/main/java/com/unal/cronus/model/entitity/StudentHasSubject.java
@@ -0,0 +1,4 @@
+package com.unal.cronus.model.entitity;
+
+public class StudentHasSubject {
+}
--- src/main/java/com/unal/cronus/model/repository/StudentRepository.java
+++ src/main/java/com/unal/cronus/model/repository/StudentRepository.java
@@ -1,9 +1,17 @@
 package com.unal.cronus.model.repository;
 
 import com.unal.cronus.model.entitity.Student;
+import com.unal.cronus.model.entitity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,String> {
+    @Query(value ="SELECT * FROM student_has_subject where student_email=:studentEmail AND subject_code=subjectCode",
+            nativeQuery = true)
+    Object searchSubjectsByName(@Param("studentEmail") String studentEmail, @Param("subjectCode") int subjectCode);
 }
