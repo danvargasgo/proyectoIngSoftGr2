aa9d7521445110976042d7c5e1205cb18aca01e1
sarodriguezcam <sarodriguezca@unal.edu.co>
2022-01-16 00:03:41 -0500

    Agregada búsqueda por código, actualizada llave compuesta grupo

--- src/main/java/com/unal/cronus/model/entitity/LlaveGrupo.java
+++ src/main/java/com/unal/cronus/model/entitity/LlaveGrupo.java
@@ -1,7 +1,12 @@
 package com.unal.cronus.model.entitity;
 
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@NoArgsConstructor
+@EqualsAndHashCode
 public class LlaveGrupo implements Serializable {
     private int number;
     private Subject subject;
